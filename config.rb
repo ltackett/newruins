d2d77edab45e09ee50e8f28d5460fc3a982eef27
ltackett <lorin.tackett@gmail.com>
2013-01-24 14:33:07 -0800

    Init styling

--- config.rb
+++ config.rb
@@ -60,7 +60,7 @@ set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
 
-set :images_dir, 'img'
+set :images_dir, 'images'
 
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true,
